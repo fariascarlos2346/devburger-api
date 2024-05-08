22e527982712df8ea6e054e2e4ce515066083bac
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-05-08 11:52:29 -0300

    servidor roda mais os dados no insomnia não aparecem

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,8 @@
 import { Router } from 'express';
-
-
-
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/',async (request, response) => {
-
-});
+routes.post('/user', UserController.store);
 
 export default routes;
